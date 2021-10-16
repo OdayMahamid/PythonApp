11319c11ee2f29959a636be51598c83ef39b97af
oday shibli <oday.shibli.99@gmail.com>
2021-10-16 16:40:21 +0300

    fix the average

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-
 from flask import Flask
 import requests
-import time
+
 
 app = Flask(__name__)
 
 
 @app.route('/BitcoinPrice')
 def BitcoinPrice():
-    timeout = time.time() + 60
-    sumPrice = 0
-    count=0
-    while time.time() < timeout:
-        count = count+1
-        # request's response
-        response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        # get the http request's response as a json object
-        data = response.json()
-        USDprice = (data["bpi"]["USD"]["rate"])
-        CurrentPrice = USDprice.replace("$", "").replace(",", "")
-        sumPrice = sumPrice + float(CurrentPrice)
+    SumPrices = 0
+    # request's response
+    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    # get the http request's response as a json object
+    data = response.json()
+    CurrentPrice = data["bpi"]["USD"]["rate"]
+    # request's response
+    response2 = requests.get('https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&limit=10')
+    # get the http request's response as a json object
+    DataLast10minutes = response2.json()
+    for i in range(10):
+        # sum the prices in approximately in one minute
+        SumPrices = SumPrices + (DataLast10minutes['Data']["Data"][i]['close'])
+    # average in last 10 minutes
+    AvgPrice = SumPrices / 10
 
-    AvgPrice = sumPrice / count
-    return "Average USD Bitcoin Price: " + str(AvgPrice)+"$<br/>"+" Current USD Bitcoin Price: " + CurrentPrice+"$"
+    return "Average USD Bitcoin Price: " + '{:,.2f}'.format(AvgPrice)+ "$<br/>" + " Current USD Bitcoin Price: " + CurrentPrice + "$"
 
 
 if __name__ == '__main__':
